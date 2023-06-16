ed6ef04c526ba0a128e36cde6315db2ccee6adf4
John Pusey <ebardx@gmail.com>
2023-06-16 18:26:43 -0400

    Prepared (mostly) for Swift 6

--- Package.swift
+++ Package.swift
@@ -4,10 +4,17 @@
 
 import PackageDescription
 
+let swiftSettings: [SwiftSetting] = [.enableUpcomingFeature("BareSlashRegexLiterals"),
+                                     .enableUpcomingFeature("ConciseMagicFile"),
+                                     .enableUpcomingFeature("ExistentialAny"),
+                                     .enableUpcomingFeature("ForwardTrailingClosures"),
+                                     .enableUpcomingFeature("ImplicitOpenExistentials")]
+
 let package = Package(name: "XestiNetwork",
                       platforms: [.iOS(.v14),
                                   .macOS(.v11)],
                       products: [.library(name: "XestiNetwork",
                                           targets: ["XestiNetwork"])],
-                      targets: [.target(name: "XestiNetwork")],
+                      targets: [.target(name: "XestiNetwork",
+                                        swiftSettings: swiftSettings)],
                       swiftLanguageVersions: [.version("5")])
